d5ab4e6e8be766dc1513e47bf47f73b8168a1f97
NehaG991 <55118296+NehaG991@users.noreply.github.com>
2020-05-25 14:39:27 -0500

    Endless Background
    Make background endless

---
+++ Hack and Slash/Assets/Scripts/EndlessBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessBackground : MonoBehaviour
+{
+    // fields
+    private float length;
+    private float startpos;
+    public GameObject cam;
+    public float parallexEffect;
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallexEffect));
+        float dist = (cam.transform.position.x * parallexEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
