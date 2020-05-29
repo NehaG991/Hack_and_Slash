c3e353df31e3a9e0d8599d94e770325f4a3a91dd
NehaG991 <55118296+NehaG991@users.noreply.github.com>
2020-05-29 16:43:46 -0500

    Character Selection
    Created the Character Selection Scene
    Camera will follow selected character

--- Hack and Slash/Assets/Scripts/CameraFollow.cs
+++ Hack and Slash/Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,38 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // fields
-    public GameObject player;
+    private GameObject[] players;
+    private GameObject player;
+    private int selectedPlayer;
 
+    private void Start()
+    {
+        // creates player array and adds the different characters to it
+        players = new GameObject[2];
+        players[0] = GameObject.FindGameObjectWithTag("Knight");
+        players[1] = GameObject.FindGameObjectWithTag("Archer");
+
+        // gets the current selected character 
+        selectedPlayer = PlayerPrefs.GetInt("CharacterSelected");
+        Debug.Log(selectedPlayer);
+
+        // the player field is equal to the current player selected
+        if (selectedPlayer == 0)
+        {
+            player = players[0];
+        }
+
+        if (selectedPlayer == 1)
+        {
+            player = players[1];
+        }
+
+    }
+
+    // camera follows current selected character
     private void LateUpdate()
     {
+        
         Vector3 temp = transform.position;
 
         temp.x = player.transform.position.x;
@@ -21,7 +49,7 @@ public class CameraFollow : MonoBehaviour
 
         transform.position = temp;
 
-        
-
     }
+
+
 }
---
+++ Hack and Slash/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    // fields
+    // List of Characters 
+    private GameObject[] characterList;
+    private int index = 0;
+
+    // start method
+    private void Start()
+    {
+        index = PlayerPrefs.GetInt("CharacterSelected");
+
+        characterList = new GameObject[transform.childCount];
+        
+        // puts characters in the array
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            characterList[i] = transform.GetChild(i).gameObject;
+        }
+
+        // makes characters invisible
+        foreach (GameObject go in characterList)
+        {
+            go.SetActive(false);
+        }
+
+        // toggle selected character
+        if (characterList[index])
+        {
+            characterList[index].SetActive(true);
+        }
+    }
+
+    // left arrow click function
+    public void ToggleLeft()
+    {
+        // toggle off current model
+        characterList[index].SetActive(false);
+
+        index--;
+        // if at beginning of list, go to end of list
+        if (index < 0)
+        {
+            index = characterList.Length - 1;
+        }
+
+        // toggle new model one
+        characterList[index].SetActive(true);
+            
+    }
+
+    // right arrow click function
+    public void ToggleRight()
+    {
+        // toggle current model off
+        characterList[index].SetActive(false);
+
+        index++;
+        // if at beginning of list, go to end of list
+        if (index > characterList.Length - 1)
+        {
+            index = 0;
+        }
+
+        // toggle new model one
+        characterList[index].SetActive(true);
+    }
+
+    // select button function
+    public void SelectButton()
+    {
+        PlayerPrefs.SetInt("CharacterSelected", index);
+        SceneManager.LoadScene("Game");
+    }
+}
